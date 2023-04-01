5a99e00b600cdc844839a09ae15aed1c863f4f52
Jane <evgenia.kochukova@gmail.com>
2023-04-01 09:28:19 -0300

    Create tests for Player

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -3,10 +3,68 @@ package ru.netology;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PlayerTest {
+    private Map<Player, List<Game>> initPlayer() {
+        Map<Player, List<Game>> sup = new HashMap<>();
+
+        Player player = new Player("Petya");
+        String name = player.getName();
+        GameStore googlePlay = new GameStore();
+
+        Game mario = new Game("Mario", "arcade", googlePlay);
+        Game warcraft = new Game("Warcraft", "strategy", googlePlay);
+        Game heroes = new Game("Heroes3", "strategy", googlePlay);
+
+        player.installGame(mario);
+        player.installGame(warcraft);
+        player.installGame(heroes);
+
+        player.play(mario, 10);
+        player.play(warcraft, 100);
+        player.play(heroes, 50);
+
+        List<Game> listGames = new ArrayList<>();
+        listGames.add(mario);
+        listGames.add(warcraft);
+        listGames.add(heroes);
+        sup.put(player, listGames);
+
+        return sup;
+    }
 
     @Test
-    public void shouldSumGenreIfOneGame() {
+    public void createNewPlayer() {
+        String expected = "Petya";
+        Player player = new Player(expected);
+
+        String actual = player.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotChangePlayedTimeAfterReinstall() {
+        Map<Player, List<Game>> playerMap = initPlayer();
+
+        Player petya = playerMap.keySet().iterator().next();
+        List<Game> games = playerMap.get(petya);
+
+        int expectedTime = petya.sumGenre("strategy");
+        petya.installGame(games.get(0));
+        petya.installGame(games.get(1));
+        petya.installGame(games.get(2));
+        int actualTime = petya.sumGenre("strategy");
+
+        assertEquals(expectedTime, actualTime);
+
+    }
+
+    @Test
+    public void shouldSumGenreIfOneGame1() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
@@ -19,5 +77,18 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldReturnTimeMostPlayedByGenre() {
+        Map<Player, List<Game>> playerMap = initPlayer();
+
+        Player petya = playerMap.keySet().iterator().next();
+        List<Game> games = playerMap.get(petya);
+
+        Game i = petya.mostPlayerByGenre("strategy");
+        String expected = "warcraft";
+        String actualName = i.getTitle();
+
+        assertEquals(expected, actualName);
+
+    }
 }
