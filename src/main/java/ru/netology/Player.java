23c8d356c029efe2071f0abf060f1e15a769c9a1
Alena <pluwa@mail.ru>
2023-04-05 01:20:23 +0400

    New class added

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -39,9 +39,9 @@ public class Player {
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, playedTime.get(game) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new NotInstalledException(game);
         }
         return playedTime.get(game);
     }
@@ -55,9 +55,10 @@ public class Player {
         for (Game game : playedTime.keySet()) {
             if (game.getGenre().equals(genre)) {
                 sum += playedTime.get(game);
-            } else {
-                sum = 0;
             }
+//            else {
+//                sum = 0;
+//            }
         }
         return sum;
     }
